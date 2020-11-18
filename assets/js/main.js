a091ad024385fb6a83cedb8dfb0519a3f8aaa6b0
Andrea <dauriaandrea99@gmail.com>
2020-11-18 16:45:53 +0100

    Exercise ! END  !

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
-// dichiarazione variabile e richiesta cognome
+// Dichiarazione variabile e richiesta cognome
 var cognome = prompt("Inserisci Il tuo Cognome");
-// dichiarazione array cognomi
+// Imposizione del cognome di almeno una lettera
+while (cognome.length < 1) {
+    var cognome = prompt("inserisci un cognome valido")
+}
+//Trasformazione del cognome per accettare qualsiasi Maiuscola e Minuscola
+var cognomeUpper = cognome.charAt(0).toUpperCase() + cognome.slice(1);
+// Dichiarazione array cognomi
 var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 // Inserimento del nuovo cognome nella lista 
-listaCognomi.push(cognome);
+listaCognomi.push(cognomeUpper);
 // Ordine alfabetico dell'array
-listaOrdinata = listaCognomi.sort();
-console.log(listaOrdinata);
+var listaOrdinata = listaCognomi.sort();
+// Visualizzazione lista ordinata 
+for (var i = 0; i<listaOrdinata.length; i++) {
+    var item = listaOrdinata[i];
+    var listaContent = document.getElementById("lista").innerHTML;
+    document.getElementById("lista").innerHTML = listaContent + "<li>" + item + "</li>";
+}
+// Visualizzazione Posizione Umana
+document.getElementById("posizione").innerHTML = " La tua posizione è: " + (listaCognomi.indexOf(cognomeUpper) + 1);
